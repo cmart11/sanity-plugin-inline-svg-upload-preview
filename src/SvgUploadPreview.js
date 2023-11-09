b2fb5735fc587e16f4aa30f32ca79068e80edfa8
Cristian <2cristianmartinez@gmail.com>
2023-11-09 19:39:58 -0300

    convert to v3

--- src/SvgUploadPreview.js
+++ src/SvgUploadPreview.js
@@ -1,85 +1,62 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { PatchEvent, set, unset } from 'part:@sanity/form-builder/patch-event';
-import FormField from 'part:@sanity/components/formfields/default';
+import React, { useCallback } from 'react';
 import SanitizedSVG from './sanitizedSvg';
 import styles from './SvgUploadPreview.css';
+import { Stack } from '@sanity/ui';
+import { set, unset } from 'sanity';
 
-class SvgStringInput extends React.Component {
-  inputRef = React.createRef();
+const SvgStringInput = (props) => {
+  const { onChange, value = '', type, level } = props;
 
-  static propTypes = {
-    value: PropTypes.string,
-    type: PropTypes.object,
-    level: PropTypes.number,
-    onChange: PropTypes.func,
+  const handleChange = useCallback(
+    (event) => {
+      const file = event.target.files?.[0];
+      if (file?.type !== 'image/svg+xml') {
+        // eslint-disable-next-line no-alert
+        window.alert(`The type of the selected file is not SVG: ${file?.type}`);
+        return;
+      }
+      const reader = new FileReader();
+      reader.onload = (readerEvent) => {
+        onChange(set(readerEvent.target.result));
+      };
+      reader.readAsText(file);
+    },
+    [onChange]
+  );
+
+  const clickedRemoveSvg = () => {
+    if (window.confirm('Are you sure you want to remove the SVG?')) {
+      onChange(unset());
+    }
   };
 
-  generateId() {
-    const title = this.props.type.title.replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) => index == 0 ? word.toLowerCase() : word.toUpperCase()).replace(/\s+/g, '')
-    return `svg-upload--${Math.random().toString()}--${title}`
-  }
-
-  handleChange = event => {
-    const file = event.target.files[0];
-    if (file.type !== 'image/svg+xml') {
-      // eslint-disable-next-line no-alert
-      window.alert(`The type of the selected file is not svg: ${file.type}`);
-      return;
-    }
-    const reader = new FileReader();
-    reader.onload = readerEvent => {
-      this.props.onChange(PatchEvent.from(set(readerEvent.target.result)));
-    };
-    reader.readAsText(file);
-  };
-
-  focus() {
-    this.inputRef.current.focus();
-  }
-
-  clickedRemoveSvg() {
-    if (confirm('Are you sure you want to remove the SVG?')) {
-      this.props.onChange(PatchEvent.from(unset()))
-    }
-    return false
-  }
-
-  render() {
-    const { value, type, level } = this.props;
-    const id = this.generateId();
-
-    return (
-      <FormField label={type.title} level={level} description={type.description}>
-        <div className={`${styles.svgPreviewBackground} ${value && styles.hasValue}`}>
-          <input
-            accept=".svg"
-            id={id}
-            ref={this.inputRef}
-            type="file"
-            placeholder={type.placeholder}
-            onChange={this.handleChange}
-            name="svg_upload"
-            onSubmit={this.onSubmit}
-          />
-          <label htmlFor={id}>Upload SVG</label>
-
-          {value && (
-            <div className={styles.svgWrapper}>
-              <SanitizedSVG html={value} />
-              <button
-                className={styles.updateSvg}
-                type="button"
-                onClick={() => this.clickedRemoveSvg()}
-              >
-                Remove SVG
-              </button>
-            </div>
-          )}
-        </div>
-      </FormField>
-    );
-  }
-}
+  return (
+    <Stack space={3}>
+      <input
+        accept=".svg"
+        type="file"
+        onChange={handleChange}
+        name="svg_upload"
+        style={{ display: 'none' }}
+      />
+      <label htmlFor="uploadInput">Upload SVG</label>
+      
+      <div className={`${styles.svgPreviewBackground} ${value && styles.hasValue}`}>
+        {value && (
+          <div className={styles.svgWrapper}>
+            <SanitizedSVG html={value} />
+            <button
+              className={styles.updateSvg}
+              type="button"
+              onClick={clickedRemoveSvg}
+            >
+              Remove SVG
+            </button>
+          </div>
+        )}
+      </div>
+    </Stack>
+  );
+};
 
 export default SvgStringInput;
